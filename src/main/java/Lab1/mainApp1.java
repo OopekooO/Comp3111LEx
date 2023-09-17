64f2adb8dfe9d2bb822d6a0ce426db6fbf214395
poonyui <poonyui2002@gmail.com>
2023-09-17 22:02:47 +0800

    2nd change

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -2,7 +2,7 @@ package Lab1;
 
 public class mainApp1 {
     public static void main(String[] args) {
-        int b = 2;
+        int b = 3;
         int n = 11;
         System.out.println(b + " power to " + n + " = " +myLibrary.Power(b, n));
     }
